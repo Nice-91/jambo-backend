88c197eb1f823c4045c5f1dc11b0cf698cd0951c
Nice-91 <iradukundanice91@gmail.com>
2025-11-04 17:26:06 +0200

    Add root route for health check

--- backend/urls.py
+++ backend/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
+
+
+def home(request):
+    return JsonResponse({"message": "Jambo Backend is running "})
 
 
 urlpatterns = [
