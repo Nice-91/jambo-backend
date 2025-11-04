ad0be768d18384f6fed908a4047ad30fbed470a6
Nice-91 <iradukundanice91@gmail.com>
2025-11-04 04:03:20 +0200

    Fix WhiteNoise middleware typo

--- backend/settings.py
+++ backend/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
 # Middleware
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',  
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
